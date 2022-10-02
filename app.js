2c7f72dd0f3e7a31974e912b737cfb9eab9bbb1c
tarun-balwani <balwani.t@northeastern.edu>
2022-10-01 22:13:08 -0400

    Adding front end code for maximum functionalities

--- app.js
+++ app.js
@@ -5,19 +5,31 @@ const { graphqlHTTP } = require("express-graphql");
 const { buildSchema } = require("graphql");
 // we can write objects in plain text and then the above package will convert it to javascript objects
 const mongoose = require("mongoose");
-const isAuth = require('./middleware/is-auth');
+const isAuth = require("./middleware/is-auth");
+const graphQlSchema = require("./graphql/schema/index");
+const graphQlResolvers = require("./graphql/resolvers/index");
 
 const app = express();
 
-const graphQlSchema = require('./graphql/schema/index');
-const graphQlResolvers = require('./graphql/resolvers/index');
+
 app.use(bodyParser.json());
+
+
+//This portion of code allows the user to perform cross origin operations
+// it basically add the headers to the incoming request
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "content-type,Authorization");
+  if(req.method === 'OPTIONS'){
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 app.use(isAuth);
-
-
-
 // const events = [];
-// app.get('/', (req,res,next) => { 
+// app.get('/', (req,res,next) => {
 //     res.send("Hello World!");
 // })
 
@@ -33,7 +45,7 @@ app.use(
 
     //Rootvalue are the resolvers and contains all the logical stuff
     // Resolvers should contain resolver names exactly same as that of the query/mutation, for ex events in rootQuery
-    rootValue:graphQlResolvers,
+    rootValue: graphQlResolvers,
     graphiql: true,
     //graphiql = true to get UI to test API
   })
@@ -44,8 +56,8 @@ mongoose
     `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@event-app.odlamac.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`
   )
   .then(() => {
-    app.listen(3000, () => {
-      console.log("server started at port 3000");
+    app.listen(8000, () => {
+      console.log("server started at port 8000");
     });
   })
   .catch((err) => {
